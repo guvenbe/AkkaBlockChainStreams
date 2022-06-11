96a926c11677732d46b7d081f5f43270067c0d80
Bora Guven <guvenbe@gmail.com>
2022-06-11 15:18:35 -0400

    mine using actors and store result

--- src/main/java/streaming/Main.java
+++ src/main/java/streaming/Main.java
@@ -3,12 +3,15 @@ package streaming;
 import akka.NotUsed;
 import akka.actor.typed.ActorSystem;
 import akka.stream.Attributes;
-import akka.stream.javadsl.Flow;
-import akka.stream.javadsl.Sink;
-import akka.stream.javadsl.Source;
+import akka.stream.FanInShape2;
+import akka.stream.FlowShape;
+import akka.stream.UniformFanOutShape;
+import akka.stream.javadsl.*;
+import akka.stream.typed.javadsl.ActorFlow;
 import blockchain.ManagerBehavior;
 import blockchain.MiningSystemBehavior;
 import model.Block;
+import model.HashResult;
 import model.Transaction;
 import utils.BlockChainUtils;
 
@@ -24,38 +27,57 @@ public class Main {
     private static Random random = new Random();
 
     public static void main(String[] args) {
-        ActorSystem<ManagerBehavior.Command> actorSystem= ActorSystem.create(MiningSystemBehavior.create(),
+        ActorSystem<ManagerBehavior.Command> actorSystem = ActorSystem.create(MiningSystemBehavior.create(),
                 "BlockChainMiner");
 
         Source<Transaction, NotUsed> transactionSource = Source.repeat(1)
                 .throttle(1, Duration.ofSeconds(1))
-                .map(x ->{
+                .map(x -> {
                     transId++;
                     System.out.println("Received transaction " + transId);
                     return new Transaction(transId, System.currentTimeMillis(), random.nextInt(1000),
-                            random.nextDouble() * 100 );
+                            random.nextDouble() * 100);
                 });
-        Flow<Transaction, Block, NotUsed> blockBuilder = Flow.of(Transaction.class).map(trans->{
+        Flow<Transaction, Block, NotUsed> blockBuilder = Flow.of(Transaction.class).map(trans -> {
             List<Transaction> list = new ArrayList<>();
             list.add(trans);
             Block block = new Block("0", list);
             System.out.println("Created the block " + block);
             return block;
-        }).conflate((block1, block2) ->{
+        }).conflate((block1, block2) -> {
             block1.addTransactionToList(block2.getFirstTransaction());
             System.out.println("Conflated the block" + block1);
             return block1;
         });
-        Flow<Block, Block, NotUsed> miningProcess = Flow.of(Block.class).map( block ->{
-            System.out.println("Starting to mine block " + block.toString());
-            Thread.sleep(10000);
-            System.out.println("Finished mining block " + block.toString());
-        return block;
-        });
+        Flow<Block, HashResult, NotUsed> miningProcess = ActorFlow
+                .ask(actorSystem, Duration.ofSeconds(30), (block, self) ->
+                        new ManagerBehavior.MineBlockCommand(block, self, 5)
+                );
+
+        Flow<Block, Block, NotUsed> miningFlow = Flow.fromGraph(
+                GraphDSL.create(builder -> {
+                    UniformFanOutShape<Block, Block> broadcast = builder.add(Broadcast.create(2));
+                    FlowShape<Block, HashResult> mineBlock = builder.add(miningProcess);
+                    //partial graph
+                    FanInShape2<Block,HashResult,Block> receiveHashResult =
+                            builder.add(ZipWith.create((block, hashResult) ->{
+                                block.setHash(hashResult.getHash());
+                                block.setNonce(hashResult.getNonce());
+                                return block;
+                            }));
+                    builder.from(broadcast)
+                            .toInlet(receiveHashResult.in0());
+                    builder.from(broadcast)
+                            .via(mineBlock)
+                            .toInlet(receiveHashResult.in1());
+                    return FlowShape.of(broadcast.in(), receiveHashResult.out());
+                })
+        );
+
 
         transactionSource
                 .via(blockBuilder)
-                .via(miningProcess.async().addAttributes(Attributes.inputBuffer(1,1)))
+                .via(miningFlow.async().addAttributes(Attributes.inputBuffer(1, 1)))
                 .to(Sink.foreach(System.out::println))
                 .run(actorSystem);
     }
