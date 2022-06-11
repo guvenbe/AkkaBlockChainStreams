9ba3ed7b5c9a4e4d03b260ec639e22994262865c
Bora Guven <guvenbe@gmail.com>
2022-06-11 03:08:43 -0400

    receive transactions with back pressure

--- src/main/java/streaming/Main.java
+++ src/main/java/streaming/Main.java
@@ -2,12 +2,19 @@ package streaming;
 
 import akka.NotUsed;
 import akka.actor.typed.ActorSystem;
+import akka.stream.Attributes;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import blockchain.ManagerBehavior;
 import blockchain.MiningSystemBehavior;
+import model.Block;
 import model.Transaction;
+import utils.BlockChainUtils;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 
@@ -28,5 +35,28 @@ public class Main {
                     return new Transaction(transId, System.currentTimeMillis(), random.nextInt(1000),
                             random.nextDouble() * 100 );
                 });
+        Flow<Transaction, Block, NotUsed> blockBuilder = Flow.of(Transaction.class).map(trans->{
+            List<Transaction> list = new ArrayList<>();
+            list.add(trans);
+            Block block = new Block("0", list);
+            System.out.println("Created the block " + block);
+            return block;
+        }).conflate((block1, block2) ->{
+            block1.addTransactionToList(block2.getFirstTransaction());
+            System.out.println("Conflated the block" + block1);
+            return block1;
+        });
+        Flow<Block, Block, NotUsed> miningProcess = Flow.of(Block.class).map( block ->{
+            System.out.println("Starting to mine block " + block.toString());
+            Thread.sleep(10000);
+            System.out.println("Finished mining block " + block.toString());
+        return block;
+        });
+
+        transactionSource
+                .via(blockBuilder)
+                .via(miningProcess.async().addAttributes(Attributes.inputBuffer(1,1)))
+                .to(Sink.foreach(System.out::println))
+                .run(actorSystem);
     }
 }
